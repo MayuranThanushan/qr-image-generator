169c65eee412bc9fa2f4e31aa13e127927bdae1e
Mayuran Thanushan <mayuranthanushan@gmail.com>
2024-07-17 18:12:19 +0530

    updated the JS code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [temp, setTemp] = useState("");
+  const [word, setWord] = useState("");
+  const size = 300;
+  const bgColor = "ffffff";
+  const [qrCode, setQrCode] = useState("");
+
+  useEffect(() => {
+    if (word) {
+      setQrCode(`https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(word)}&size=${size}x${size}&bgcolor=${bgColor}`);
+    }
+  }, [word, size, bgColor]);
+
+  function handleClick() {
+    setWord(temp);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>QR Image Generator</h1>
+      <div className="input-box">
+        <div className="gen">
+          <input
+            type="url"
+            onChange={(e) => setTemp(e.target.value)}
+            placeholder="Enter URL to encode"
+            value={temp}
+          />
+          <button className="button" onClick={handleClick}>
+            Generate
+          </button>
+        </div>
+      </div>
+
+      <div className="output-box">
+        {qrCode && (
+          <>
+            <img src={qrCode} alt="QR Code" />
+            <a href={qrCode} download="QRImage">
+              <button type="button">Download</button>
+            </a>
+          </>
+        )}
+      </div>
     </div>
   );
 }
